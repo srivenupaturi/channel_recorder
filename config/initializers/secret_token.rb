a40867a28b3c13328bf25d9daaaab135ab0c5e80
srivenupaturi <srivenu_paturi@yahoo.com>
2014-07-25 22:35:24 -0400

    initial_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChannelRecorder::Application.config.secret_token = '64fa9f9412d9d978300ad9a96026d3d065e1c2b2e4adedfe1642929b1532df4b2d3ba8045c0dfa11560e19f1029991c4aa0efdc9cc437988f66dcc9991f11503'
